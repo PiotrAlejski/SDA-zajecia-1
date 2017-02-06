ba81eb7e3798da8b1583770900adf9fd1079f696
unknown <piotr.alejski@gmail.com>
2017-02-06 19:49:51 +0100

    added methods: triangle, square, tree, oneMatrix

---
+++ src/zajecia/piate/zajecia5.java
@@ -0,0 +1,66 @@
+package zajecia.piate;
+
+/**
+ * Created by RENT on 2017-02-06.
+ */
+public class zajecia5 {
+
+    public static void main(String[] args) {
+        //   square(3);
+        //   trinangle(4);
+        // tree(10);
+        oneMATRIX(10);
+    }
+
+    public static void oneMATRIX(int number) {
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                if (i == j) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void trinangle(int number) {
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void square(int number) {
+        for (int i = 0; i < number; i++) {     //wiersze
+            for (int j = 0; j < number; j++) { //kolumny
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void tree(int number) {
+        for (int i = 0; i < number; i++) {             //ilość wiersza
+            for (int k = 0; k < number - i - 1; k++) { //wyświtla spacje
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (i * 2) + 1; j++) {   //wyświetla gwiazdki
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j <= (number - 1); j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+    }
+
+}
