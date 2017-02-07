2c451cca740ccda850b17ee5db5c416ad595b2cc
unknown <piotr.alejski@gmail.com>
2017-02-07 18:00:46 +0100

    added homework

--- src/zajecia/piate/zajecia5.java
+++ src/zajecia/piate/zajecia5.java
@@ -1,5 +1,7 @@
 package zajecia.piate;
 
+import zajecia.czwarte.ZadaniaZeScannerem;
+
 /**
  * Created by RENT on 2017-02-06.
  */
@@ -9,8 +11,25 @@ public class zajecia5 {
         //   square(3);
         //   trinangle(4);
         // tree(10);
-        oneMATRIX(10);
+        // oneMATRIX(10);
+        homework ();
     }
+
+    public static int[] homework() {
+        int[] array = new int[10];
+        boolean flag = true;
+        while(flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            if (numberFromUser >= 0 && numberFromUser <= 9) {
+                array[numberFromUser]++;
+            }else {
+                flag = false;
+            }
+        }
+        return array;
+    }
+
+
 
     public static void oneMATRIX(int number) {
         for (int i = 0; i < number; i++) {
