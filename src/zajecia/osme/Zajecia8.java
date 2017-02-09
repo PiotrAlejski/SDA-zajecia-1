3b00346eb64352b5f81eaadf0d91812deaede7db
unknown <piotr.alejski@gmail.com>
2017-02-09 20:41:58 +0100

    added 2 part of zajecia8

--- src/zajecia/osme/Zajecia8.java
+++ src/zajecia/osme/Zajecia8.java
@@ -8,9 +8,60 @@ public class Zajecia8 {
      //   System.out.println(factorial(100));
      //   System.out.println(factorialRecursion(8));
     //   System.out.println(FibonacciNumber(10));
-        System.out.println(FibonacciNumberRec(6));
-
+     //   System.out.println(FibonacciNumberRec(6));
+     //   String message = "ALa Ma Kota";
+     //   System.out.println(switchCase(message));
+     //   System.out.println(sumOfNumberDigital(525273));
+        System.out.println(startsWith2("Ala ma kota", "Ala"));
     }
+
+    //can throw exception
+    public static boolean startsWith1(String message, String expression) {
+        return message.startsWith(expression);
+    }
+
+    public static boolean startsWith2(String message, String expression) {
+        return message.substring(0, expression.length()) .equals(expression);
+    }
+
+    public static int sumOfNumberDigital(int number){
+        int sum = 0;
+        while(number != 0){
+            sum += number%10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+
+    public static String switchCase(String message) {
+        char[] charArray = message.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] >= 65 && charArray[i] <= 90) {
+                charArray[i] += 32;
+            } else if (charArray[i] >= 97 && charArray[i] <= 122) {
+                charArray[i] -= 32;
+            }
+        }
+        return String.valueOf(charArray);
+    }
+
+      public static String swithCaseWithBuilde(String message) {
+          StringBuilder stringBuilder = new StringBuilder();
+          for (int i = 0; i < message.length(); i++) {
+              char tmp = message.charAt(i);
+              if (tmp >= 65 && tmp <= 90) {
+                  stringBuilder.append((char)(tmp + 32));
+              }else if (tmp >= 97 && tmp <= 122) {
+                  stringBuilder.append((char) (tmp - 32));
+              }else {
+                  stringBuilder.append(tmp);
+              }
+          }
+          return stringBuilder.toString();
+
+      }
+
 
     public static int FibonacciNumberRec(int index) {
         if (index == 0) {
